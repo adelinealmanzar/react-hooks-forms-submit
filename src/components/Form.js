ae47f19992732d38bb623ea1da18f6bba5fa8038
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-03 16:26:18 -0800

    commit tree dbab2adcc4b0d9986cdaa51b4ce02614b3f2d997

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([])
+  const [error, setError] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,11 +14,40 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (firstName.length > 1) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      }
+      const dataArr = [...submittedData, formData]
+      setSubmittedData(dataArr)
+      setFirstName('')
+      setLastName('')
+      setError([])
+    } else {
+      setError(['First name please'])
+    }
+  }
+
+  const renderSubmittedData = submittedData.map(el => {
+      return (
+        <div key={el.firstName}>
+          {el.firstName} {el.lastName}
+        </div>
+      )
+    }
+  )
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      <h3>Submitted Data</h3>
+      {renderSubmittedData}
+      {error.length > 0 ? error.map(er => <div key={er[0]}>{er}</div>) : null }
     </form>
   );
 }
